a83b1254e9293582e200a9abd85066117176c625
GUHARAJ G G <g.g.guharaj@gmail.com>
2025-11-28 15:17:40 +0000

    new

--- models/intermediate/int_suppliers.sql
+++ models/intermediate/int_suppliers.sql
@@ -1,6 +1,5 @@
 {{config(
     tags = 'sample'
-
 )}}
 
 with suppliers as (
@@ -18,7 +17,7 @@ regions as (
     from {{ ref('stg_regions') }}
 )
 
-select s.* exclude (nation_id),n.name as nation_name,r.name as region_name
+select s.* exclude (nation_id),n.name as nation_name,r.name as region_name,
 
 from suppliers s
 join nations n
